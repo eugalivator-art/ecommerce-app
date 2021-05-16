d3be615b6b72f6fcf7e48f848ce21afbff7664f3
eugalivator-art <mohanacharya3299@gmail.com>
2021-05-16 20:13:55 +0530

    cartitem

--- client/src/components/CartItem.tsx
+++ client/src/components/CartItem.tsx
@@ -1,47 +1,55 @@
 import React from 'react'
 
-type Props = {
+type CartProps = {
     prodName: string,
+    prodSP: string,
+    prodStock: number,
+    prodId: number,
+    prodsTP: number,
+    prodQuantity: number,
+    currencyCode: string,
+    incrementClick: (id: number) => void,
+    decrementClick: (id: number) => void,
+    removeItem: (id: number) => void,
     
 }
-function CartItem() {
+const CartItem: React.FC<CartProps> = ({currencyCode, incrementClick, decrementClick, removeItem,prodId, prodSP, prodName, prodStock, prodQuantity, prodsTP }) =>  {
     return (
-        <div></div>
-        // <div className="card d-flex allign-center border-primary w-20 text-center rounded">
-        //         <div className="card-header bg-primary text-white">
-        //             <h4 className="text-uppercase">{val.productName}</h4>
-        //         </div>
+        <div className="card d-flex allign-center border-primary w-20 text-center rounded">
+                <div className="card-header bg-primary text-white">
+                    <h4 className="text-uppercase">{prodName}</h4>
+                </div>
                                 
-        //         <div className="card-body ">
-        //             <h5 className="card-title">Description- The details of the product</h5>
-        //             <p className="card-text">Price - { val.productSalePrice}</p>
-        //             <p className="card-text"><small> {val.productStock} available in stock</small></p>
+                <div className="card-body ">
+                <p className="card-text mb-1">Price - {prodSP} { currencyCode}</p>
+                    <p className="card-text"><small> {prodStock} available in stock</small></p>
 
-        //             <div>
-        //                 <button
-        //                     className="btn btn-sm border-primary mx-auto my-2"
-        //                     onClick={() => this.props.increament(val.productId)}>+</button>
-        //                 {val.productQty}
-        //                 <button
-        //                     className="btn btn-sm mx-auto border-primary my-2"
-        //                     onClick={() => this.props.decreament(val.productId)}>-</button>
-        //             </div>
+                    <div>
+                        <button
+                            className="btn btn-sm border-primary mx-auto my-2"
+                            onClick={() => incrementClick(prodId)}>+</button>
+                        {prodQuantity}
+                        <button
+                            className="btn btn-sm mx-auto border-primary my-2"
+                            onClick={() => decrementClick(prodId)}>-</button>
+                    </div>
+                    
+                    <div>{prodsTP} { currencyCode} </div>
                         
-        //             <button
-        //                 onClick={(e) => {
-        //                     this.props.removeCartItem(val.productId);
-        //                     this.deduceTotal(val.productSalePrice)
-        //                 }}
-        //             className="btn btn-sm w-25 btn-danger mx-autotext-uppercase"
-        //             >
-        //             <i className="fa fa-trash"></i> Remove from Cart
-        //             </button>
+                    <button
+                        onClick={(e) => {
+                            removeItem(prodId);
+                        }}
+                    className="btn btn-sm w-25 btn-danger mx-autotext-uppercase"
+                    >
+                    <i className="fa fa-trash"></i> Remove from Cart
+                    </button>
                     
                     
-        //         </div>
+                </div>
                             
                                 
-        // </div>
+        </div>
     )
 }
 
--- client/src/containers/CartPage.tsx
+++ client/src/containers/CartPage.tsx
@@ -3,6 +3,7 @@ import { connect, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Dispatch } from 'redux';
 import { validateLocaleAndSetLanguage } from 'typescript';
+import CartItem from '../components/CartItem';
 import Column from '../components/Column';
 import LoadingWrapper from '../components/LoadingWrapper';
 import Row from '../components/Row';
@@ -12,8 +13,8 @@ import { CartType, ProductType, StoreType } from '../types';
 type Props = {
     cartitems: CartType[];
     removeCartItem: (id: number) => void;
-    increament: (id: number) => void;
-    decreament: (id: number) => void;
+    increment: (id: number) => void;
+    decrement: (id: number) => void;
     currency: string
 }
 
@@ -57,45 +58,20 @@ class CartPage extends Component<Props, State> {
                 <Row>
                     {this.props.cartitems.map((val, id) => (
                         <Column size={8} classes={"my-3 p-4 text-center mx-auto rounded mt-2"}>
+
+                            <CartItem
+                                prodName={val.productName}
+                                prodSP={val.productPrice}
+                                prodStock={val.productStock}
+                                prodId={val.productId}
+                                prodQuantity={val.productQty}
+                                prodsTP={val.productTotalPrice}
+                                currencyCode={this.props.currency}
+                                incrementClick={(id) => this.props.increment(id)}
+                                decrementClick={(id) => this.props.decrement(id)}
+                                removeItem = {(id) => this.props.removeCartItem(id)}
+                            ></CartItem>
                             
-
-                            <div className="card d-flex allign-center border-primary w-20 text-center rounded">
-                                <div className="card-header bg-primary text-white">
-                                <h4 className="text-uppercase">{val.productName}</h4>
-                                </div>
-                                
-                                <div className="card-body ">
-                                    <p className="card-text mb-1">Price - {val.productSalePrice} { this.props.currency}</p>
-                                    <p className="card-text mb-1"><small> {val.productStock} available in stock</small></p>
-
-                                    <div>
-                                        
-                                        
-                                        <button
-                                            className="btn btn-sm mx-auto border-primary my-2"
-                                            onClick={() => this.props.decreament(val.productId)}>-</button>
-                                        {val.productQty}
-                                        <button
-                                            className="btn btn-sm border-primary mx-auto my-2"
-                                            onClick={(e) => this.props.increament(val.productId)}>+</button>
-                                    </div>
-
-                                    <div>{val.productTotalPrice} { this.props.currency} </div>
-                                        
-                                    <button
-                                        onClick={() => {
-                                            this.props.removeCartItem(val.productId);
-                                        }}
-                                    className="btn btn-sm w-25 btn-danger mx-autotext-uppercase"
-                                    >
-                                    <i className="fa fa-trash"></i> Remove from Cart
-                                    </button>
-                                    
-                                    
-                                </div>
-                            
-                                
-                            </div>
                         </Column>
                     ))}
 
@@ -123,8 +99,8 @@ const mapStoreToProps = (store: StoreType) => {
 const mapDispatchToProps = (dispatch: Dispatch) =>  {
     return {
         removeCartItem: (id: number) => dispatch(CartActions.removeItem(id)),
-        increament: (id:number ) => dispatch(CartActions.increaseQuantity(id)),
-        decreament: (id:number ) => dispatch(CartActions.decreaseQuantity(id))
+        increment: (id:number ) => dispatch(CartActions.increaseQuantity(id)),
+        decrement: (id:number ) => dispatch(CartActions.decreaseQuantity(id))
     }
 }
 
